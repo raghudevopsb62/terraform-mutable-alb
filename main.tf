14b9406f6d5463eddfc8559acd648849927f839b
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-02-25 08:55:26 +0530

    Update terraform-modules/terraform-mutable-alb/

--- main.tf
+++ main.tf
@@ -13,3 +13,28 @@ resource "aws_lb" "backend" {
   enable_deletion_protection = false
   subnets                    = ["subnet-0b673cb0f74714420", "subnet-01f5793a850edca81"]
 }
+
+//module "tags" {
+//  count       = length(local.ALL_TAGS)
+//  source      = "git::https://github.com/raghudevopsb62/terraform-tags"
+//  TAG_NAME    = lookup(element(local.ALL_TAGS, count.index), "name")
+//  TAG_VALUE   = lookup(element(local.ALL_TAGS, count.index), "value")
+//  RESOURCE_ID =
+//}
+//
+//locals {
+//  ALL_TAGS = [
+//    {
+//      name  = "env"
+//      value = var.ENV
+//    },
+//    {
+//      name  = "project_name"
+//      value = "roboshop"
+//    }
+//  ]
+//}
+
+output "ALB" {
+  value = aws_lb.frontend
+}
