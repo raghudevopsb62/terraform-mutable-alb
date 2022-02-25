3e12faf82b182b9bff364279d9fe3f8f30f56163
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-02-25 08:49:55 +0530

    Update terraform-modules/terraform-mutable-alb/

--- main.tf
+++ main.tf
@@ -5,3 +5,11 @@ resource "aws_lb" "frontend" {
   enable_deletion_protection = false
   subnets                    = ["subnet-0b673cb0f74714420", "subnet-01f5793a850edca81"]
 }
+
+resource "aws_lb" "backend" {
+  name                       = "backend"
+  internal                   = false
+  load_balancer_type         = "application"
+  enable_deletion_protection = false
+  subnets                    = ["subnet-0b673cb0f74714420", "subnet-01f5793a850edca81"]
+}
