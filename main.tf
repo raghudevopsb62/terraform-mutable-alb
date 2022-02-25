c2ff0e213b880cc2835516f62c9fafdf859d0902
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-02-25 09:01:14 +0530

    Update terraform-modules/terraform-mutable-alb/

--- main.tf
+++ main.tf
@@ -4,6 +4,10 @@ resource "aws_lb" "frontend" {
   load_balancer_type         = "application"
   enable_deletion_protection = false
   subnets                    = ["subnet-0b673cb0f74714420", "subnet-01f5793a850edca81"]
+  tags = {
+    Name = "frontend-${var.ENV}"
+    env  = var.ENV
+  }
 }
 
 resource "aws_lb" "backend" {
@@ -12,27 +16,10 @@ resource "aws_lb" "backend" {
   load_balancer_type         = "application"
   enable_deletion_protection = false
   subnets                    = ["subnet-0b673cb0f74714420", "subnet-01f5793a850edca81"]
-}
-
-module "tags" {
-  count       = length(local.ALL_TAGS)
-  source      = "git::https://github.com/raghudevopsb62/terraform-tags"
-  TAG_NAME    = lookup(element(local.ALL_TAGS, count.index), "name")
-  TAG_VALUE   = lookup(element(local.ALL_TAGS, count.index), "value")
-  RESOURCE_ID = [aws_lb.frontend.id]
-}
-
-locals {
-  ALL_TAGS = [
-    {
-      name  = "env"
-      value = var.ENV
-    },
-    {
-      name  = "project_name"
-      value = "roboshop"
-    }
-  ]
+  tags = {
+    Name = "frontend-${var.ENV}"
+    env  = var.ENV
+  }
 }
 
 variable "ENV" {}
