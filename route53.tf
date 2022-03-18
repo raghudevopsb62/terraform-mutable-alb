ac37e06dce8d3d02e1be147910762991c0e2c711
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-03-18 08:22:22 +0530

    Update terraform-modules/terraform-mutable-alb/

--- route53.tf
+++ route53.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "frontend" {
   zone_id = data.terraform_remote_state.vpc.outputs.PUBLIC_HOSTED_ZONEID
-  name    = "roboshop-${var.ENV}.${data.terraform_remote_state.vpc.outputs.PUBLIC_HOSTED_ZONE_NAME}"
+  name    = "roboshop-${var.ENV}"
   type    = "CNAME"
   ttl     = "300"
   records = [aws_lb.frontend.dns_name]
