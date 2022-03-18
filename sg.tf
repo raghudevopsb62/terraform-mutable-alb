0964cf97a20f575fd5bd0a56e1533d47cedfb2dd
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-03-18 08:34:32 +0530

    Update terraform-modules/terraform-mutable-alb/

--- sg.tf
+++ sg.tf
@@ -11,6 +11,14 @@ resource "aws_security_group" "allow-public" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "HTTP"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port        = 0
     to_port          = 0
